1876b0a7a9d9bb8eef0dcd1358e644d5fff90a2b
elizabetlg <elizabetlg77@gmail.com>
2023-11-22 23:10:33 -0500

    DocumentTypes Ok

--- NeoSmart/NeoSmart.UnitTest/Controllers/DocumentTypesControllerTests.cs
+++ NeoSmart/NeoSmart.UnitTest/Controllers/DocumentTypesControllerTests.cs
@@ -47,18 +47,6 @@ namespace NeoSmart.UnitTest.Controllers
             _mockDbContext.Dispose();
         }
 
-        [TestMethod]
-        public async Task GetComboAsync_ReturnsOkResult()
-        {
-            // Arrange
-
-            // Act
-            var result = await _controller.GetComboAsync() as OkObjectResult;
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual(200, result.StatusCode);
-
-        }
+       
     }
 }
