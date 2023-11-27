24e3fd9c0ed5892ba873721fa21067645ace179e
elizabetlg <elizabetlg77@gmail.com>
2023-11-27 13:54:38 -0500

    ProcessesControllerTests test method GetComboAsync_ReturnsOkResult

---
+++ NeoSmart/NeoSmart.UnitTest/Controllers/ProcessesControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NeoSmart.BackEnd.Controllers;
+using NeoSmart.BackEnd.Interfaces;
+using NeoSmart.ClassLibraries.Entities;
+using NeoSmart.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoSmart.UnitTest.Controllers
+{
+    [TestClass]
+    public class ProcessesControllerTests
+    {
+        private readonly DbContextOptions<DataContext> _options;
+        private readonly Mock<IGenericUnitOfWork<Process>> _unitOfWorkMock;
+        public ProcessesControllerTests()
+        {
+            _options = new DbContextOptionsBuilder<DataContext>()
+     .UseInMemoryDatabase(Guid.NewGuid().ToString())
+     .Options;
+            _unitOfWorkMock = new Mock<IGenericUnitOfWork<Process>>();
+        }
+
+        [TestMethod]
+        public async Task GetComboAsync_ReturnsOkResult()
+        {
+            // Arrange
+            var context = new DataContext(_options);
+            var controller = new ProcessesController(_unitOfWorkMock.Object, context);
+
+            // Act
+            var result = await controller.GetComboAsync() as OkObjectResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+
+            // Clean up (if needed)
+            context.Database.EnsureDeleted();
+
+        }
+
+    }
+}
