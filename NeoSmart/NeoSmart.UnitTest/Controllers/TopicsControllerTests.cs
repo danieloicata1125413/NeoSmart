ba54d7ce922406d1945a85711b14f10d2765d530
elizabetlg <elizabetlg77@gmail.com>
2023-11-27 14:53:53 -0500

    TopicsControllerTests test method GetAsync_ReturnsOkResult

---
+++ NeoSmart/NeoSmart.UnitTest/Controllers/TopicsControllerTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NeoSmart.BackEnd.Controllers;
+using NeoSmart.BackEnd.Interfaces;
+using NeoSmart.ClassLibraries.DTOs;
+using NeoSmart.ClassLibraries.Entities;
+using NeoSmart.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoSmart.UnitTest.Controllers
+{
+    [TestClass]
+    public class TopicsControllerTests
+    {
+        private readonly DbContextOptions<DataContext> _options;
+        private readonly Mock<IGenericUnitOfWork<Topic>> _unitOfWorkMock;
+        public TopicsControllerTests()
+        {
+            _options = new DbContextOptionsBuilder<DataContext>()
+     .UseInMemoryDatabase(Guid.NewGuid().ToString())
+     .Options;
+            _unitOfWorkMock = new Mock<IGenericUnitOfWork<Topic>>();
+        }
+
+        [TestMethod]
+
+        public async Task GetAsync_ReturnsOkResult()
+        {
+            // Arrange
+            using var context = new DataContext(_options);
+            var controller = new TopicsController(_unitOfWorkMock.Object, context);
+            var pagination = new PaginationDTO();
+
+            // Act
+            var result = await controller.GetAsync(pagination) as OkObjectResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+
+            // Clean up (if needed)
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ReturnsOkResult()
+        {
+            // Arrange
+            using var context = new DataContext(_options);
+            var controller = new TopicsController(_unitOfWorkMock.Object, context);
+            var pagination = new PaginationDTO();
+
+            // Act
+            var result = await controller.GetPagesAsync(pagination) as OkObjectResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+
+            // Clean up (if needed)
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ReturnsNotFoundWhenEntityNotFound()
+        {
+            // Arrange
+            using var context = new DataContext(_options);
+            var controller = new TopicsController(_unitOfWorkMock.Object, context);
+
+            // Act
+            var result = await controller.GetAsync(1) as NotFoundResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(404, result.StatusCode);
+
+            // Clean up (if needed)
+            context.Database.EnsureDeleted();
+            context.Dispose();
+        }
+
+    }
+}
