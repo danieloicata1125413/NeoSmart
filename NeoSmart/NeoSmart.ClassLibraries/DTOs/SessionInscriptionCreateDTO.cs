585bf965ed8c0b05e362e81e7a9205e62c52d667
daniel oicata hernandez <desarrollo@ati.net.co>
2024-06-03 19:26:38 -0500

    Staged Ok

---
+++ NeoSmart/NeoSmart.ClassLibraries/DTOs/SessionInscriptionCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace NeoSmart.ClassLibraries.DTOs
+{
+    public class SessionInscriptionCreateDTO
+    {
+        public int SesionId { get; set; }
+
+        public List<string>? Users { get; set; }
+    }
+}
---
+++ NeoSmart/NeoSmart.ClassLibraries/DTOs/TrainingTopicsDTO.cs
@@ -0,0 +1,14 @@
+﻿using NeoSmart.ClassLibraries.Interfaces;
+
+namespace NeoSmart.ClassLibraries.DTOs
+{
+    public class TrainingTopicsDTO : ISoftDetete
+    {
+        public int Id { get; set; }
+
+        public List<int>? TrainingTopicIds { get; set; }
+
+        public ICollection<TrainingTopicDTO>? TrainingTopics { get; set; }
+
+    }
+}
